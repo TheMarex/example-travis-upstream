f94c202fe0e455e3270c17dda85ce1a96a45db62
Patrick Niklaus <patrick.niklaus@student.kit.edu>
2016-04-10 03:12:02 +0200

    Inline complete script

--- scripts/trigger-downstream.sh
+++ scripts/trigger-downstream.sh
@@ -7,6 +7,56 @@ TRAVIS_COMMIT=${TRAVIS_COMMIT:-$(git log --format=%H --no-merges -n 1 | tr -d '\
 TRAVIS_BRANCH=${TRAVIS_BRANCH:-$(git rev-parse --abbrev-ref HEAD)}
 DOWNSTREAM_BRANCH=${DOWNSTREAM_BRANCH:-${TRAVIS_BRANCH}}
 ESCAPED_DOWNSTREAM_REPO=$(echo ${DOWNSTREAM_REPO} | sed "s/\//%2F/g")
+DOWNSTREAM_GITHUB_MSG_PENDING="
+{
+    \"state\": \"pending\",
+    \"target_url\": \"https://travis-ci.org/${ESCAPED_DOWNSTREAM_REPO}/builds\",
+    \"description\": \"Running downstream build\",
+    \"context\": \"continuous-integration/downstream/${DOWNSTREAM_REPO}/0\"
+}"
+DOWNSTREAM_GITHUB_MSG_SUCCESS="
+{
+    \"state\": \"success\",
+    \"target_url\": \"https://travis-ci.org/${ESCAPED_DOWNSTREAM_REPO}/builds\",
+    \"description\": \"Running downstream build\",
+    \"context\": \"continuous-integration/downstream/${DOWNSTREAM_REPO}/\${TRAVIS_BUILD_NUMBER}\"
+}"
+DOWNSTREAM_GITHUB_MSG_FAILURE="
+{
+    \"state\": \"failure\",
+    \"target_url\": \"https://travis-ci.org/${ESCAPED_DOWNSTREAM_REPO}/builds\",
+    \"description\": \"Running downstream build\",
+    \"context\": \"continuous-integration/downstream/${DOWNSTREAM_REPO}/\${TRAVIS_BUILD_NUMBER}\"
+}"
+
+PENDING_PAYLOAD="
+echo \"Status [\${TRAVIS_BUILD_NUMBER}]: For ${TRAVIS_COMMIT} in ${UPSTREAM_REPO}\"; \
+curl -s -X POST \
+  -H \"Content-Type: application/json\" \
+  -H \"Accept: application/json\" \
+  -H \"Authorization: token \${GITHUB_TOKEN}\" \
+  -d \"${DOWNSTREAM_GITHUB_MSG_PENDING}\" \
+  \"https://api.github.com/repos/${UPSTREAM_REPO}/statuses/${TRAVIS_COMMIT}\" > /dev/null
+"
+FAILURE_PAYLOAD="
+echo \"Status [\${TRAVIS_BUILD_NUMBER}]: For ${TRAVIS_COMMIT} in ${UPSTREAM_REPO}\"; \
+curl -s -X POST \
+  -H \"Content-Type: application/json\" \
+  -H \"Accept: application/json\" \
+  -H \"Authorization: token \${GITHUB_TOKEN}\" \
+  -d \"${DOWNSTREAM_GITHUB_MSG_FAILURE}\" \
+  \"https://api.github.com/repos/${UPSTREAM_REPO}/statuses/${TRAVIS_COMMIT}\" > /dev/null
+"
+SUCCESS_PAYLOAD="
+echo \"Status [\${TRAVIS_BUILD_NUMBER}]: For ${TRAVIS_COMMIT} in ${UPSTREAM_REPO}\"; \
+curl -s -X POST \
+  -H \"Content-Type: application/json\" \
+  -H \"Accept: application/json\" \
+  -H \"Authorization: token \${GITHUB_TOKEN}\" \
+  -d \"${DOWNSTREAM_GITHUB_MSG_SUCCESS}\" \
+  \"https://api.github.com/repos/${UPSTREAM_REPO}/statuses/${TRAVIS_COMMIT}\" > /dev/null
+"
+
 TRAVIS_MSG="
 {
 \"request\":
@@ -14,20 +64,16 @@ TRAVIS_MSG="
         \"message\": \"Triggered build: ${UPSTREAM_REPO} : ${TRAVIS_COMMIT}\",
         \"branch\": \"${DOWNSTREAM_BRANCH}\",
         \"config\": {
-            \"before_script\": \"export TRIGGER_COMMIT=${TRAVIS_COMMIT}\"
+            \"before_script\": "${PENDING_PAYLOAD}"
+            \"after_success\": "${SUCCESS_PAYLOAD}"
+            \"after_failure\": "${FAILURE_PAYLOAD}"
         }
     }
 }"
-GITHUB_MSG="
-{
-    \"state\": \"pending\",
-    \"target_url\": \"https://travis-ci.org/${ESCAPED_DOWNSTREAM_REPO}/builds\",
-    \"description\": \"Running downstream build\",
-    \"context\": \"continuous-integration/downstream/${DOWNSTREAM_REPO}/0\"
-} "
 
 trigger_downstream() {
     echo "Triggering: ${DOWNSTREAM_BRANCH} in ${DOWNSTREAM_REPO}"
+    echo "${TRAVIS_MSG}"
     curl -s -X POST \
       -H "Content-Type: application/json" \
       -H "Accept: application/json" \
@@ -37,15 +83,4 @@ trigger_downstream() {
       "https://api.travis-ci.org/repo/${ESCAPED_DOWNSTREAM_REPO}/requests" > /dev/null
 }
 
-post_pending() {
-    echo "Status [0]: For ${TRAVIS_COMMIT} in ${UPSTREAM_REPO}"
-    curl -s -X POST \
-      -H "Content-Type: application/json" \
-      -H "Accept: application/json" \
-      -H "Authorization: token ${GITHUB_TOKEN}" \
-      -d "${GITHUB_MSG}" \
-      "https://api.github.com/repos/${UPSTREAM_REPO}/statuses/${TRAVIS_COMMIT}" > /dev/null
-}
-
 trigger_downstream
-#post_pending
